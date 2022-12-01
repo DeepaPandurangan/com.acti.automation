b0078db6056406da387ab1b10b7fefb7954661c2
deepa <deepa@192.168.1.43>
2022-12-01 10:55:52 -0500

    added TaskPage and Methods for Creating Customer

--- src/main/java/com/acti/pages/TaskPage.java
+++ src/main/java/com/acti/pages/TaskPage.java
@@ -1,5 +1,85 @@
 package com.acti.pages;
 
-public class TaskPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import com.acti.base.DriverScript;
+
+/*
+ * Name: TaskPage
+ * Description: Task page elements and actions
+ * Developed By; Deepa
+ * Date:29//11/22
+ * Reviewed By: shantosh
+ */
+
+       public class TaskPage extends DriverScript{
+
+	//page elements
+	 @FindBy(xpath="//div[text()='Add New']") WebElement addNewButton;
+	 @FindBy(xpath="//div[text()='+ New Customer']") WebElement newCustomerItem;	
+	 @FindBy(xpath="(//input[@placeholder='Enter Customer Name'])[2]") WebElement customernameTextBox;
+	 @FindBy(xpath="//textarea[@placeholder='Enter Customer Description']") WebElement customerdescriptionTextarea;
+	 @FindBy(xpath="(//div[text()='Create Customer'])[6]") WebElement createcustomerButton;
+	 @FindBy(xpath="//span[@class='innerHtml']") WebElement successMessage;
+	  
+	 
+	 //page initialization
+	
+	    public TaskPage()
+		{
+			PageFactory.initElements(driver, this);
+		}
+	
+	//page Method/Actions
+	    
+	    public void clickAddNewButton() 
+	    {
+	    	addNewButton.click();
+	    }
+	
+	    public void clickNewCustomerItem()
+	    {
+	    	newCustomerItem.click();
+	    }
+	
+	    public void enterCustomerName(String customername)
+	    {
+	    	
+			customernameTextBox.sendKeys(customername);
+			
+	    }
+	
+	    public void enterCustomerDescription(String custdesc) 
+	    {
+	    	customerdescriptionTextarea.sendKeys(custdesc);
+	    }
+	
+	    public void clickCreateCustomer() 
+	    {
+	    	createcustomerButton.click();
+	    }
+	    
+	    public String getSuccessMessage() 
+	    {
+	    	return successMessage.getText();
+	    }
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	
+	
+	
+	
+	
+	
+	
+	
 }
