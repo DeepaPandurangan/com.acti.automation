3a242f4f264632e99c38f3b6812bd15406fb96a9
deepa <deepa@192.168.1.43>
2022-12-01 11:21:35 -0500

    Moved the common code to a BaseTest class

--- src/test/java/com/acti/testcase/LoginPagesTests.java
+++ src/test/java/com/acti/testcase/LoginPagesTests.java
@@ -1,71 +1,65 @@
 package com.acti.testcase;
 
-
-
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.acti.base.DriverScript;
 import com.acti.pages.EnterPage;
 import com.acti.pages.LoginPage;
 
-public class LoginPagesTests extends DriverScript{
+public class LoginPagesTests extends BaseTest {
+	LoginPage lp;
+	EnterPage ep;
 
+	@BeforeMethod
+	public void setup() 
+	{
+		initApplication();
+		lp = new LoginPage();
+		EnterPage ep = new EnterPage();
+	}
+
+	@AfterMethod
+	public void teardown()
+	{
+		quitDriver();
+	}
+
+	@Test
+	public void testLoginPageTitle() 
+	{
+		String expected = "actiTIME - Login";
+		String actual = lp.getLoginPageTitle();
+		Assert.assertEquals(actual, expected);
+    }
+
+	@Test
+	public void testActiImageIsDisplayed()
+     {
+        boolean flag = lp.verifyActiLogo();
+		Assert.assertTrue(flag);
+      }
+
+	@Test
+	public void testForgotpasswordIsDisplayed() 
+	{
+        boolean flag = lp.verifyforgotpassword();
+		Assert.assertTrue(flag);
+      }
+
+	@Test
+	public void testLoginFunction() 
+	{
+        lp.enterUsername("admin");
+		lp.enterpassword("manager");
+		lp.clickLogin();
+		String expected = "John Doe";
+		String actual = ep.verifyUserLoggedIn();
+		Assert.assertEquals(actual, expected);
+		ep.clickLogout();
+	}
 	
-@Test
-public void testLoginPageTitle()
-{
-	initApplication();
-	LoginPage lp = new LoginPage();
-	String expected = "actiTIME - Login";
-	String actual = lp.getLoginPageTitle();
-	Assert.assertEquals(actual,expected);
-	quitDriver();
-}
-@Test
-public void testActiImageIsDisplayed() 
-{
-	initApplication();
-	LoginPage lp = new LoginPage();
-	boolean flag = lp.verifyActiLogo();
-	Assert.assertTrue(flag);
-	quitDriver();
-}
-@Test
-public void testForgotpasswordIsDisplayed()
-{
-	initApplication();
-	LoginPage lp = new LoginPage();
-	 boolean flag = lp.verifyforgotpassword();
-	 Assert.assertTrue(flag);
-	 quitDriver();
-}
-@Test
-public void testLoginFunction()
-{
-	initApplication();
-	LoginPage lp = new LoginPage();
-	lp.enterUsername("admin");
-	lp.enterpassword("manager");
-	lp.clickLogin();
-	EnterPage ep = new EnterPage();
-	String expected= "John Doe";
-	String actual = ep.verifyUserLoggedIn();
-	Assert.assertEquals(actual,expected);
-	ep.clickLogout();
-	quitDriver();
-}
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
